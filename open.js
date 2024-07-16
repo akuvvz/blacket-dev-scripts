9ddd26a1a87569ae71787e1079b7e4257769963f
aku <165962602+akuvvz@users.noreply.github.com>
2024-07-15 23:38:53 -0500

    🤓🤓

--- open.js
+++ open.js
@@ -4,4 +4,4 @@ setInterval(() => fetch2.post("/api/market/open-pack", { packId: 1 }).then(() =>
 setInterval(() => fetch2.put("/api/blooks/sell-blooks", { blookId: 2, quantity: blooksUnlocked }).then(() => {
 	console.info(`Sold ${blooksUnlocked} blooks`);
 	blooksUnlocked = 0;
-}), 50000);
+})), 50000);

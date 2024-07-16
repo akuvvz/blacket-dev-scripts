f14ba8e0a68d6591bd625bb27a7ee486e78117f4
aku <165962602+akuvvz@users.noreply.github.com>
2024-07-15 23:38:13 -0500

    final fix

--- open.js
+++ open.js
@@ -1,4 +1,7 @@
 let blooksUnlocked = 0;
 
 setInterval(() => fetch2.post("/api/market/open-pack", { packId: 1 }).then(() => blooksUnlocked = blooksUnlocked + 1;), 700);
-setInterval(() => fetch2.put("/api/blooks/sell-blooks", { blookId: 2, quantity: blooksUnlocked }).then(() => console.info(`Sold ${blooksUnlocked} blooks`)), 50000);
+setInterval(() => fetch2.put("/api/blooks/sell-blooks", { blookId: 2, quantity: blooksUnlocked }).then(() => {
+	console.info(`Sold ${blooksUnlocked} blooks`);
+	blooksUnlocked = 0;
+}), 50000);
